cfd362a3d4f6334c3faafd2bd28c2b3755b14418
Robert Fowler <agelessfitness@protonmail.com>
2026-05-10 21:44:08 -0500

    fix consts export

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SeamossInsider = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'SeamossInsider';
+export const SITE_DESCRIPTION = 'Wellness from the Sea';
